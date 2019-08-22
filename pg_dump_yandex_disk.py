ef3e181056e97cb9945d75c142df9c61fcc7749d
Alexander Kakushin <alexandrkakushin@yandex.ru>
2019-08-22 17:40:53 +0300

    Дампы сохраняются в подкаталог вида YYYY-mm

--- pg_dump_yandex_disk.py
+++ pg_dump_yandex_disk.py
@@ -16,12 +16,16 @@ def run():
     # Создание каталога с дампами
     yadisk.mkdir(conf.yandex.dir)
 
+    # Создание каталога YYYY-mm
+    dir_dumps = conf.yandex.dir + "/%s" % datetime.datetime.now().strftime('%Y-%m')
+    yadisk.mkdir(dir_dumps)
+
     file_name = '/tmp/pg_dump_yandex_disk.sql.tar'
     # Создание дампа
     pg.dump(file_name)
 
     yadisk.upload(file_name, "%s/%s.sql.tar" %
-                  (conf.yandex.dir, datetime.datetime.now().strftime('%Y-%m-%d_%H_%M_%S')))
+                  (dir_dumps, datetime.datetime.now().strftime('%Y-%m-%d_%H_%M_%S')))
 
 
 run()
